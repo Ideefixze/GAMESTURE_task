0689a25386b4181a9e2338ac1dbd24c24a70067a
Ideefixze <kaczooorqu@gmail.com>
2021-02-23 14:52:40 +0100

    Added popup class

---
+++ GamestureTask/Assets/Scripts/GUI/Popup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Base class for all Popup UI objects.
+/// </summary>
+public class Popup : MonoBehaviour
+{
+    public TMP_Text MainText;
+
+    //Close Button should not be changed
+    [field:SerializeField]
+    public Button CloseButton { get; private set; }
+
+    void Start()
+    {
+        InitPopup();
+    }
+
+    void InitPopup()
+    {
+        //Close Button functionality initialization
+        CloseButton.onClick.AddListener(() => Destroy(gameObject));
+    }
+}
