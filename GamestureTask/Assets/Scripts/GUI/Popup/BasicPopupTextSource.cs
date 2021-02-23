05201da744f18b5da8a71796d7782fe7f3166273
Ideefixze <kaczooorqu@gmail.com>
2021-02-23 15:47:28 +0100

    Added: popup displaying and popup text source

---
+++ GamestureTask/Assets/Scripts/GUI/Popup/BasicPopupTextSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPopupTextSource : MonoBehaviour, IPopupTextSource
+{
+    [SerializeField]
+    [Multiline]
+    private string DefaultText;
+    public string GetPopupText()
+    {
+        return DefaultText;
+    }
+    
+}
---
+++ GamestureTask/Assets/Scripts/GUI/Popup/PopupDisplayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Displays a popup in the middle of the screen by instantiating a displayerPopup object.
+/// </summary>
+
+[RequireComponent(typeof(IPopupTextSource))]
+public class PopupDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject displayedPopup;
+
+    private IPopupTextSource popupTextSource;
+
+    public void Start()
+    {
+        popupTextSource = GetComponent<IPopupTextSource>();
+    }
+
+    public void DisplayPopup()
+    {
+        Popup popup = Instantiate(displayedPopup)?.GetComponent<Popup>();
+
+        if (popup == null) throw new MissingComponentException("Failed to display Popup. Check if DisplayedPopup has a Popup component.");
+
+        //Set up Popup position and parent
+        popup.GetComponent<RectTransform>().position = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        popup.transform.parent = FindObjectOfType<Canvas>().transform;
+
+        //Set up Popup text
+        popup.MainText.text = popupTextSource.GetPopupText();
+    }
+}
